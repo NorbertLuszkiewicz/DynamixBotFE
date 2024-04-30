3ad64157eaa459acbee953367bc2f0a4f042aa6e
Norbert Luszkiewicz <norbert.luszkiewicz@stxnext.pl>
2024-04-30 13:44:23 +0200

    Update twitch scopes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   url: 'https://dynamix-bot.glitch.me/',
   loginRedirect:
-    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamix-bot.glitch.me/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions moderation:read moderator:manage:banned_users user:read:subscriptions user:read:email user:write:chat user:read:chat channel:read:predictions channel:manage:predictions chat:edit chat:read&state=localhost',
+    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamix-bot.glitch.me/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions moderation:read moderator:manage:banned_users user:read:subscriptions user:read:chat user:write:chat user:read:chat channel:read:predictions channel:manage:predictions chat:edit chat:read&state=localhost',
 };
