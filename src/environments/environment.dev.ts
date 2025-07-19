800308b717e62293f71488765f2d4a1901de53b9
Norbert Luszkiewicz <norbertluszkiewicz00@gmail.com>
2025-07-19 21:38:20 +0200

    change server url from glitch to onreader

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  url: 'https://dynamix-bot.glitch.me/',
+  url: 'https://dynamixbot.onrender.com/',
   loginRedirect:
-    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamix-bot.glitch.me/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions moderation:read moderator:manage:banned_users user:read:subscriptions user:read:chat user:write:chat user:read:chat channel:read:predictions channel:manage:predictions chat:edit chat:read&state=localhost',
+    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamixbot.onrender.com/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions moderation:read moderator:manage:banned_users user:read:chat user:write:chat user:read:chat channel:read:predictions channel:manage:predictions chat:edit chat:read&state=localhost',
 };
